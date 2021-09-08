67c30c1488dba260469f7b6fdba550862ca15a98
EC2 Default User <ec2-user@ip-172-31-28-53.ap-northeast-1.compute.internal>
2021-09-08 11:58:48 +0000

    c5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,13 @@
 class TasksController < ApplicationController
   def new
+    @task = Task.new
+  end
+  
+  def index
+    @task = Task.all
+  end
+  
+  def show
+    @task = Task
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/new'
-
+  
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   
