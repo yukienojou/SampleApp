ef963e002d86c5edb2d8a21362f41314cc74efe6
EC2 Default User <ec2-user@ip-172-31-28-53.ap-northeast-1.compute.internal>
2021-09-11 13:15:59 +0000

    タスク編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,28 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+    
   end
+  
+  def show
+   @task = Task.find(params[:id])
+  end
+  
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+     
+      redirect_to @task
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+    def task_params
+      params.require(:task).permit(:name, :detail)
+    end
 end
+  
