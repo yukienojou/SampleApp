702941c69c0e2c8f252f5488f3d7956d30d95d33
EC2 Default User <ec2-user@ip-172-31-28-53.ap-northeast-1.compute.internal>
2021-09-21 14:07:29 +0000

    編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   root 'static_pages#top'
   get '/signup', to: 'users#new'
-  
+  get 'tasks/:id/edit', to:'tasks#edit', as: :edit_task_path
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
