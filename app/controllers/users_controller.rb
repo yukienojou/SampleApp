80618efafcfe540f0d905f52228b55004ef4fdac
EC2 Default User <ec2-user@ip-172-31-28-53.ap-northeast-1.compute.internal>
2021-10-02 15:11:14 +0000

    E4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,4 +75,13 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to root_url unless current_user.admin?
     end
+    
+      # 管理権限者、または現在ログインしているユーザーを許可します。
+    def admin_or_correct_user
+      @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません。"
+        redirect_to(root_url)
+      end  
+    end
 end
