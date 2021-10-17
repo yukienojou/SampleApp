fc40e4af317cce8b030217138434a74838ed9cd7
EC2 Default User <ec2-user@ip-172-31-28-53.ap-northeast-1.compute.internal>
2021-10-17 12:16:11 +0000

    task エラー

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :set_task, only: %i(show edit update destroy)
+  before_action :set_user 
+  before_action :set_task 
   before_action :logged_in_user
   before_action :current_user
  
@@ -26,7 +26,7 @@ class TasksController < ApplicationController
   
   
   def create
-    @task = @user.tasks.new(task_params)
+    @task = @user.new.tasks.new(task_params) 
     if @task.save
       flash[:success] = '新規作成に成功しました。'
       redirect_to user_tasks_url @user
